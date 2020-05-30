f04f4ffe6bec02e8ee2d573af6d4ed3d75ef5269
Alexis Bellido <alexis@ventanazul.com>
2020-05-30 12:48:58 -0400

    try char array

--- src/Simple.java
+++ src/Simple.java
@@ -3,11 +3,11 @@ import static java.lang.Math.*;
 public class Simple {
     public static void main(String[] args) {
         double x = 4;
-        double y = Math.sqrt(x);
+        double y = sqrt(x);
         System.out.println("square root: " + y);
-        double z = Math.pow(2, 4);
+        double z = pow(2, 4);
         System.out.println(z);
-        System.out.println(Math.log10(1000));
+        System.out.println(log10(1000));
 
         // this is possible thanks to import static
         System.out.println(pow(3, 4));
@@ -17,8 +17,18 @@ public class Simple {
         System.out.println(justInt);
 
         // to round to nearest integer
-        int nearestInt = (int) Math.round(original);
+        int nearestInt = (int) round(original);
         System.out.println(nearestInt);
+
+        char[] letters = new char[3];
+        letters[0] = 'a';
+        letters[1] = 'b';
+        letters[2] = 'c';
+        System.out.printf("%s %s %s%n", letters[0], letters[1], letters[2]);
+
+        String miniAlpha = new String(letters);
+        System.out.println(miniAlpha);
+
 
     }
 }
